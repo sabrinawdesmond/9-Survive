5c304485f11a1f8e364bcdf1201457535d3322b4
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-03-09 17:06:27 -0500

    changes

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-!function(){"use strict";var e;e=class{constructor(e){this.ele=e,this.ele.innerHTML="<h1>It's ALIVEEEE!!!</h1>",this.handleClick=this.handleClick.bind(this),this.ele.addEventListener("click",this.handleClick)}handleClick(){this.ele.children[0].innerText="Ouch!"}},document.addEventListener("DOMContentLoaded",(()=>{const t=document.getElementById("main");new e(t)}))}();
+!function(){"use strict";var e;e=class{constructor(e){this.ele=e,this.ele.innerHTML="<h1>WHYYYYYYYYYYY</h1>",this.handleClick=this.handleClick.bind(this),this.ele.addEventListener("click",this.handleClick)}handleClick(){this.ele.children[0].innerText="Ouch!"}},document.addEventListener("DOMContentLoaded",(()=>{const n=document.getElementById("main");new e(n)}))}();
 //# sourceMappingURL=main.js.map
