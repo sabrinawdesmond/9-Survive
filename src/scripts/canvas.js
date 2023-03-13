1a4838d0ab221d960fb3704b3204744539784e95
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-03-13 10:00:45 -0400

    background image added

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,17 +1,19 @@
+//centering canvas
 const canvas = document.querySelector('canvas')
 canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border:2px solid blue";
+let ctx = canvas.getContext('2d');
+
+//background image
+var background = new Image();
+background.src = "src/assets/background2.png";
+
+background.onload = function(){
+  ctx.drawImage(background,0,0,canvas.width, canvas.height);   
+}
+
 
 canvas.width = window.innerWidth * 0.8;
 canvas.height = window.innerHeight *  0.8;
-
-
-
-let ctx = canvas.getContext('2d');
-
-window.addEventListener('resize', function () {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-})
 
 
 let midx = canvas.width/2
@@ -27,8 +29,6 @@ function Circle (x, y, radius) {
   this.draw = function() {
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    // ctx.strokeStyle = "purple"
-    // ctx.stroke()
     ctx.fillStyle = "blue"
     ctx.fill()
   }
@@ -46,11 +46,11 @@ function drawEnemies() {
     ctx.fillRect(x, y, enemySize, enemySize);
   }
 }
-  
-let circle = new Circle(midx, midy, 30)
+
+let circle = new Circle(midx, midy, 40)
 
 const enemySize = 50;
-const enemyColor = 'red';
+const enemyColor = 'black';
 const enemyPositions = [
   // starting positions
   { x: 100, y: 100 },
@@ -59,14 +59,21 @@ const enemyPositions = [
   { x: 800, y: 300 },
 ];
 
-function Enemies(x, y, dx, dy, height, width, color) {
-  this.x = x
-  this.y = y
-  this.dx = dx
-  this.dy = dy
-  this.height = height
-  this.width = width
-  this.color = color
-}
+// function Enemies(x, y, dx, dy, height, width, color) {
+//   this.x = x
+//   this.y = y
+//   this.dx = dx
+//   this.dy = dy
+//   this.height = height
+//   this.width = width
+//   this.color = color
+// }
 
 
+// function animate() {
+//   requestAnimationFrame(animate)
+//   ctx.clearRect(0,0,innerWidth, innerHeight)
+
+//   if ()
+// }
+// animate()
